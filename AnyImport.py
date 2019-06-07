1d642c5a1262d0c3dc4b7fb7a31d8fb3e24a8c93
J White <cw@mekanation.com>
2019-06-06 20:19:13 -0500

    File import Setup

---
+++ AnyImport.py
@@ -0,0 +1,31 @@
+# //ToDo
+# 1. Take in file
+#   1A - Get file headers
+# 2. Get api url
+# 3. Get Api Headers
+# 4. Map file to API headers  
+#5. Push data to api
+#
+#
+#
+
+
+
+###Imports
+import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
+
+
+
+
+
+
+### Asks for file
+
+root = Tkinter.Tk()
+root.withdraw() ##remove this when mapping portion is ready //Todo
+
+file_path = filedialog.askopenfilename()
+
+
