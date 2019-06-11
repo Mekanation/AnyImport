7712e5d47968768f80c320e0f96bcd6de55f7d30
J White <cw@mekanation.com>
2019-06-10 22:30:43 -0500

    Json Partial setup

--- AnyImport.py
+++ AnyImport.py
@@ -4,7 +4,7 @@
 # 2. Get api url
 # 3. Get Api Headers
 # 4. Map file to API headers 
-#   4A - Use SQLalchemy as an in memory staging table. 
+#   4A - Use SQLalchemy as an in memory staging table. Maybe. 
 #5. Push data to api
 #
 #
@@ -16,8 +16,9 @@
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
-
-
+from requests import request
+import json
+from pandas.io.json import json_normalize
 
 
 
@@ -30,5 +31,19 @@ file_path = filedialog.askopenfilename() #gets file path
 
 source = pd.read_excel(file_path) #converts file to dataframe
 *source_cols, = source #gets local file headers
+
+#init json request variables
+instance = input("What instance? ")
+base_url =".repfabric.com/yoxel_sync/v1.2/"
+companies = "companies"
+contacts = "contacts"
+
+#start Json
+response=request(url="http://"+instance+base_url+companies, method='get')
+metadata = response.json()
+metadata_loaded = json.loads(metadata)
+results = json_normalize(metadata_loaded)
+print(results)
+
 
 print(source_cols)
